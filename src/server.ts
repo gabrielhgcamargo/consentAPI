d01e6bc3e536c0712eca5f7f46a3c4ca68e68fd6
gabrielhgcamargo <gacamargos4@gmail.com>
2022-12-14 10:48:18 -0300

    swagger comment

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,16 @@ import express from "express";
 import { routes } from "./routes";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
-import swaggerDocs from "./swagger.json";
+//import swaggerDocs from "./swagger.json";
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
-app.use(
-  "/open-insurance/v1/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerDocs)
-);
+// app.use(
+//   "/open-insurance/v1/api-docs",
+//   swaggerUi.serve,
+//   swaggerUi.setup(swaggerDocs)
+// );
 
 app.listen(8000, () => console.log("Server online on port 8000"));
